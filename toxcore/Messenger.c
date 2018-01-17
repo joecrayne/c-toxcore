4a2b96c5601cadbe52504977f666ca71d8ecf1a7
ingvar1995 <igorn1995@yandex.ru>
2018-01-17 22:46:47 +0300

    minor fixes - oob

--- toxcore/Messenger.c
+++ toxcore/Messenger.c
@@ -2123,7 +2123,7 @@ Messenger *new_messenger(Messenger_Options *options, unsigned int *error)
         kill_networking(m->net);
         kill_net_crypto(m->net_crypto);
         kill_DHT(m->dht);
-//        kill_gca(m->group_announce);
+        kill_gca(m->group_announce);
         free(m);
         return nullptr;
     }
@@ -3466,7 +3466,7 @@ uint32_t count_friendlist(const Messenger *m)
     uint32_t i;
 
     for (i = 0; i < m->numfriends; i++) {
-        if (m->friendlist[i].status > 0) {
+        if (m->friendlist[i].status > 0 && m->friendlist[i].type != CONTACT_TYPE_GC) {
             ret++;
         }
     }
@@ -3497,7 +3497,7 @@ uint32_t copy_friendlist(Messenger const *m, uint32_t *out_list, uint32_t list_s
             break; /* Abandon ship */
         }
 
-        if (m->friendlist[i].status > 0) {
+        if (m->friendlist[i].status > 0 && m->friendlist[i].type != CONTACT_TYPE_GC) {
             out_list[ret] = i;
             ret++;
         }
