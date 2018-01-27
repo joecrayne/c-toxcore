ef939711a560451f79e700ea40dc82568f8126fa
ingvar1995 <igorn1995@yandex.ru>
2018-01-28 00:29:24 +0300

    missing ;

--- other/bootstrap_daemon/src/tox-bootstrapd.c
+++ other/bootstrap_daemon/src/tox-bootstrapd.c
@@ -259,7 +259,7 @@ int main(int argc, char *argv[])
     }
 
     Onion *onion = new_onion(dht);
-    GC_Announces_List *gc_announces_list = new_gca_list()
+    GC_Announces_List *gc_announces_list = new_gca_list();
     Onion_Announce *onion_a = new_onion_announce(dht, gc_announces_list);
 
     if (!(onion && onion_a)) {
