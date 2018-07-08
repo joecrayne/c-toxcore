46894c2a306683ee099a02300639f8d9b4abc456
ingvar1995 <igorn1995@yandex.ru>
2018-07-08 17:49:30 +0300

    various fixes for public groups announces

--- auto_tests/monolith_test.cpp
+++ auto_tests/monolith_test.cpp
@@ -273,7 +273,7 @@ int main(int argc, char *argv[])
     CHECK_SIZE(Onion_Client, 15824);
     CHECK_SIZE(Onion_Client_Cmp_data, 176);
     CHECK_SIZE(Onion_Client_Paths, 2520);
-    CHECK_SIZE(Onion_Friend, 2104);
+    CHECK_SIZE(Onion_Friend, 2112);
     CHECK_SIZE(Onion_Node, 168);
     // toxcore/onion
     CHECK_SIZE(Onion, 245832);
